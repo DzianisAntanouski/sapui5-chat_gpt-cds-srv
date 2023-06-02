99b5de792d88804ef57f2e01296dcebeb393be23
Dzianis Antanouski <dzianis.antanouski@leverx.com>
2023-06-02 07:02:41 +0000

    5

---
+++ srv/key.js
@@ -0,0 +1,7 @@
+function getKey () {
+    const OPENAI_KEY = "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";
+    return OPENAI_KEY
+}
+
+
+module.export = getKey;
---
+++ srv/openai.js
@@ -0,0 +1,26 @@
+const { Configuration, OpenAIApi } = require("openai")
+const getKey = require("./key.js")
+
+class OpenAI {
+    constructor(apiKey) {
+        const configuration = new Configuration({
+            apiKey,
+        });
+        this.openai = new OpenAIApi(configuration);
+    }
+
+    async chat(messages) {
+        try {
+            const response = await this.openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages,
+            });
+            return response.data.choices[0].message;
+        } catch (error) {
+            console.log("Err while GPT chat", error.message);
+        }
+    }
+}
+
+const openai = new OpenAI(getKey());
+module.exports = openai;
